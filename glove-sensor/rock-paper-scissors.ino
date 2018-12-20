2dcadb7fd70be9d01fe9cf8b5325c624fa34e7b3
CambridgeHandsOnScience <45903774+CambridgeHandsOnScience@users.noreply.github.com>
2018-12-20 15:20:17 +0000

    Update rock-paper-scissors.ino
    Edits for only two sensors

--- glove-sensor/rock-paper-scissors.ino
+++ glove-sensor/rock-paper-scissors.ino
@@ -6,15 +6,14 @@
 // parameters copied from TensorFlow code
 //
 
-float weights[3][3] = {
+float weights[2][3] = {
   {3.007032,-9.370312,6.363242},
   {10.726093,-0.494137,-10.231989},
-  {3.132884,-6.927588,3.794688},
 };
 
 float biases[3] = {-5.709740,6.722741,-1.013021};
 
-float scaling[3] = {0.000978,0.001600,0.001160};
+float scaling[2] = {0.000978,0.001600};
 
 //
 // main
@@ -25,7 +24,6 @@ void setup() {
   Serial.begin(9600);
   pinMode(A0, INPUT);  
   pinMode(A1, INPUT);  
-  pinMode(A2, INPUT);
   pinMode(5, OUTPUT);
 }
 
@@ -36,25 +34,23 @@ void loop() {
   delay(10);
 
   // read sensor data
-  float d[3];
+  float d[2];
   d[0] = float(analogRead(A0));
   d[1] = float(analogRead(A1));
-  d[2] = float(analogRead(A2));
 
   // print sensor data
-//  Serial.println(String(d[0]) + "," + String(d[1]) + "," + String(d[2])); 
+//  Serial.println(String(d[0]) + "," + String(d[1]); 
 //  return;
 
   // standardize data
   d[0] *= scaling[0];
   d[1] *= scaling[1];
-  d[2] *= scaling[2];
 
   // calc logits
   float lg[3];
-  lg[0] = (d[0] * weights[0][0]) + (d[1] * weights[1][0]) + (d[2] * weights[2][0]) + biases[0];
-  lg[1] = (d[0] * weights[0][1]) + (d[1] * weights[1][1]) + (d[2] * weights[2][1]) + biases[1];
-  lg[2] = (d[0] * weights[0][2]) + (d[1] * weights[1][2]) + (d[2] * weights[2][2]) + biases[2];
+  lg[0] = (d[0] * weights[0][0]) + (d[1] * weights[1][0]) + biases[0];
+  lg[1] = (d[0] * weights[0][1]) + (d[1] * weights[1][1]) + biases[1];
+  lg[2] = (d[0] * weights[0][2]) + (d[1] * weights[1][2]) + biases[2];
 
   // control servo
   if (lg[0] > lg[1] && lg[0] > lg[2]) {
